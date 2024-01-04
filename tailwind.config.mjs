98c793baef6e023c0391a2a53492d39d930d6ba4
Alfonso Ramos <alfonsoramos.1803@gmail.com>
2024-01-03 20:14:53 -0700

    [ADD] Avance: maquetado footer y header, asi como los componentes de los cursos, falta pulir detalles de los estilos

---
+++ tailwind.config.mjs
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {},
+		colors: {
+			"soft-pink": "#FFA7C3",
+			"pink": "#F74780",
+			"gray": "#83869A",
+			"deep-blue": "#13183F",
+			"steel-blue": "#666CA3",
+			"white": "#FFFFFF",
+			"gradient-pink": "#F02AA6",
+			"gradient-orange": "#FF6F48",
+			"gradient-blue": "#4851FF",
+		}
+	},
+	plugins: [],
+}
